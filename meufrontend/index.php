e6a3f86c3308d3bac13c72c7c0ced3853eebecef
wagnermarques <wagnerdocri@gmail.com>
2026-03-05 20:10:34 -0300

    ok

--- meufrontend/index.php
+++ meufrontend/index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>VM</title>
 </head>
 
 <body>
-    <h1>MEU FRONT END </h1>
+    <?php echo "<h1>MEU FRONT END EM PHP DEU CERTO!</h1>" ?>
 </body>
 
 </html>
